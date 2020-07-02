15a1881325f85889f47406214a59af0bd4e305de
h3mps <56362575+h3mps@users.noreply.github.com>
2020-07-02 12:28:47 -0400

    Update fon-REAL-webapp.py

--- fon-REAL-webapp.py
+++ fon-REAL-webapp.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 @st.cache
 def importdata():
     # store the data somewhere online; there are size limits which can be an issue, but GitHub is a good option
-    return pd.read_csv("https://raw.githubusercontent.com/h3mps/t1webapp/master/fon-REAL-data.csv")
+    return pd.read_csv("https://raw.githubusercontent.com/h3mps/fonrealwebapp/master/fon-REAL-data.csv")
 
 # call function to import the data
 df = importdata()
